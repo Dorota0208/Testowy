3b95d0cff548e5854237c4de48d9b3f6ef3f8574
Dorota Sn <dewsynow@gmail.com>
2024-10-08 21:21:51 +0200

    Dodanie tagów do testów

--- src/test/java/homework/CalculatorTest.java
+++ src/test/java/homework/CalculatorTest.java
@@ -7,54 +7,54 @@ import org.testng.annotations.Test;
 public class CalculatorTest {
     Calculator calculator = new Calculator();
 
-    @Test
+    @Test (groups = {"Addition"})
     public void testSumPositiveNumbers() {
         int result = calculator.sum(5, 10);
         Assert.assertEquals(result, 15, "Sum of 5 and 10 should be 15");
     }
 
-    @Test
+    @Test (groups = {"Addition"})
     public void testSumNegativeNumbers() {
         int result = calculator.sum(-5, -10);
         Assert.assertEquals(result, -15, "Sum of -5 and -10 should be -15");
     }
 
-    @Test
+    @Test (groups = {"Subtraction"})
     public void testSubtractPositiveNumbers() {
         int result = calculator.subtract(10, 5);
         Assert.assertEquals(result, 5, "Subtracting 5 from 10 should be 5");
     }
 
-    @Test
+    @Test (groups = {"Subtraction"})
     public void testSubtractNegativeNumbers() {
         int result = calculator.subtract(-10, -5);
         Assert.assertEquals(result, -5, "Subtracting -5 from -10 should be -5");
     }
 
-    @Test
+    @Test (groups = {"Multiplication"})
     public void testMultiplyPositiveNumbers() {
         int result = calculator.multiply(5, 10);
         Assert.assertEquals(result, 50, "Multiplication of 5 and 10 should ne 50");
     }
 
-    @Test
+    @Test (groups = {"Multiplication"})
     public void testMultiplyNegativeNumbers() {
         int result = calculator.multiply(-5, -10);
         Assert.assertEquals(result, 50, "Multiplication of -5 and -10 should be 50");
     }
 
-    @Test
+    @Test (groups = {"Division"})
     public void testDividePositiveNumbers() {
         int result = calculator.divide(10, 2);
         Assert.assertEquals(result, 5, "10 divided by 2 should be 5");
     }
-    @Test
+    @Test (groups = {"Division"})
     public void testDivideNegativeNumbers() {
         int result = calculator.divide(-10, -2);
         Assert.assertEquals(result, 5, "-10 divided by -2 should be 5");
     }
 
-    @Test
+    @Test (groups = {"Division"})
     public void testDivideByZero() {
         try {
             calculator.divide(10, 0);
